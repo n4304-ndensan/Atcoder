624ee473a00f74f0f74de70b06bb56b217f786c7
ShogoTodoroki <n4304@ndensan.co.jp>
2023-06-06 17:13:46 +0900

    create ABC085B.KagamiMochi.cpp

---
+++ BeginnerContest/360/a_a_health_breakfast.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include<string>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+ 
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vld = vector<ld>;
+using vvld = vector<vld>;
+using vst = vector<string>;
+using vvst = vector<vst>;
+ 
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+#define pq_big(T) priority_queue<T,vector<T>,less<T>>
+#define pq_small(T) priority_queue<T,vector<T>,greater<T>>
+#define all(a) a.begin(),a.end()
+#define rep(i,start,end) for(ll i=start;i<(ll)(end);i++)
+#define per(i,start,end) for(ll i=start;i>=(ll)(end);i--)
+#define uniq(a) sort(all(a));a.erase(unique(all(a)),a.end())
+
+int main() {
+    string S;
+    cin >> S;
+
+    size_t pos1 = S.find("R");
+    size_t pos2 = S.find("M");
+
+    if (pos1 < pos2) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
