c7e6950414f991bd57c2ffe6f4fa4c04738667a5
ShogoTodoroki <n4304@ndensan.co.jp>
2023-06-07 16:57:44 +0900

    ABC043

---
+++ BeginnerContest/043/D.アンバランス.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+ 
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vld = vector<ld>;
+using vvld = vector<vld>;
+using vst = vector<string>;
+using vvst = vector<vst>;
+ 
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+#define pq_big(T) priority_queue<T,vector<T>,less<T>>
+#define pq_small(T) priority_queue<T,vector<T>,greater<T>>
+#define all(a) a.begin(),a.end()
+#define rep(i,start,end) for(ll i=start;i<(ll)(end);i++)
+#define per(i,start,end) for(ll i=start;i>=(ll)(end);i--)
+#define uniq(a) sort(all(a));a.erase(unique(all(a)),a.end())
+
+int main() {
+    string s;
+    cin >> s;
+    int x, y;
+    x = -1;
+    y = -1;
+    bool a = false;
+    per(i, s.size()-1, 2){
+        per(j, s.size()-1, 0){
+            string subs;
+            subs = s.substr(j, i);
+            map<char, int> mp;
+            for(auto x : subs){
+                if(mp.find(x) != mp.end()){
+                    mp[x]++;
+                    if(mp[x] > i/2){
+                        x = j+1;
+                        y = i+j;
+                        a = true;
+                        break;
+                    }
+                }
+                else{
+                    mp[x] = 1;
+                }
+                if(a){
+                    break;
+                }
+            }
+            if(a){
+                break;
+            }
+        }
+        if(a){
+            break;
+        }
+    }
+
+    cout << x << " " << y << endl;
+}
