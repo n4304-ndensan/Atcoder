1394ebd33d20aa13999cb1217ae1be1e6402efb4
ShogoTodoroki <n4304@ndensan.co.jp>
2023-06-08 09:37:16 +0900

    create ABC.A.高橋君とホテルイージー.cpp

---
+++ BeginnerContest/044/A.高橋君とホテルイージー.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+ 
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vld = vector<ld>;
+using vvld = vector<vld>;
+using vst = vector<string>;
+using vvst = vector<vst>;
+ 
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+#define pq_big(T) priority_queue<T,vector<T>,less<T>>
+#define pq_small(T) priority_queue<T,vector<T>,greater<T>>
+#define all(a) a.begin(),a.end()
+#define rep(i,start,end) for(ll i=start;i<(ll)(end);i++)
+#define per(i,start,end) for(ll i=start;i>=(ll)(end);i--)
+#define uniq(a) sort(all(a));a.erase(unique(all(a)),a.end())
+
+int main() {
+    int N, K, X, Y;
+    cin >> N >> K >> X >> Y;
+    int ans = 0;
+    rep(i, 1, N+1){
+        if(i <= K) ans += X;
+        else ans += Y;
+    }
+
+    cout << ans << endl;
+}
