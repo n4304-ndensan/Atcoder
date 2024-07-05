e38a2001bd3ec78c8412442b00658f3205896797
轟 尚冴 <n4304@ndensan.co.jp>
2024-07-05 11:10:30 +0900

    feat: ABC359 B問題 Couples
    同じ色のTシャツを着てる人が２人、２Nの配列が与えられる、N色のTシャツが存在する。
    同じ色のTシャツに挟まれる人が１人の人が何人いるのか求める問題。

---
+++ BeginnerContest/359/b_couples.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+ 
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vld = vector<ld>;
+using vvld = vector<vld>;
+using vst = vector<string>;
+using vvst = vector<vst>;
+ 
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+#define pq_big(T) priority_queue<T,vector<T>,less<T>>
+#define pq_small(T) priority_queue<T,vector<T>,greater<T>>
+#define all(a) a.begin(),a.end()
+#define rep(i,start,end) for(ll i=start;i<(ll)(end);i++)
+#define per(i,start,end) for(ll i=start;i>=(ll)(end);i--)
+#define uniq(a) sort(all(a));a.erase(unique(all(a)),a.end())
+
+int main() {
+    int n;
+    int ans = 0;
+    cin >> n;
+    vi A(n, -1);
+    rep (i, 0, 2 * n) {
+        int a;
+        cin >> a;
+        if (A[a-1] == -1 ) {
+            A[a-1] = i;
+        } else if (A[a-1] + 2 == i) {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}
